dd81a984bf451567fc2ff7f58acffbdd7b9433f7
Samuel P. Gillispie II <spgillispie2@gmail.com>
2021-12-02 11:02:13 -0500

    Day 2 solution

---
+++ day02.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+### local imports
+import utils
+
+
+@utils.part1
+def part1(puzzleInput: str):
+    # Parse the list of instructions in tuple pairs of a string and int
+    instructions: list[tuple[str, int]] = [
+        (line.split(" ")[0], int(line.split(" ")[1]))
+        for line in puzzleInput.strip().splitlines()
+    ]
+
+    distance: int = 0
+    depth: int = 0
+
+    for direction, magnitude in instructions:
+        if direction == "forward":
+            distance += magnitude
+        elif direction == "down":
+            depth += magnitude
+        elif direction == "up":
+            depth -= magnitude
+
+    utils.printAnswer(1, distance * depth)
+
+    # Pass parsed instructions to part 2
+    return instructions
+
+
+@utils.part2
+def part2(_, instructions: list[tuple[str, int]]):
+    distance: int = 0
+    depth: int = 0
+    aim: int = 0
+
+    for direction, magnitude in instructions:
+        if direction == "forward":
+            distance += magnitude
+            depth += aim * magnitude
+        elif direction == "down":
+            aim += magnitude
+        elif direction == "up":
+            aim -= magnitude
+
+    utils.printAnswer(2, distance * depth)
+
+
+utils.start()
