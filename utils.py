86c86338295e9f3a86554a9d6faa588c3dc01b21
Samuel P. Gillispie II <spgillispie2@gmail.com>
2021-12-02 12:20:40 -0500

    Made utils internal vars scoped private

--- utils.py
+++ utils.py
@@ -11,14 +11,14 @@ import typer
 
 
 # Global variables
-part1SolutionFunc: typing.Optional[typing.Callable] = None
-part2SolutionFunc: typing.Optional[typing.Callable] = None
-solutionOptions: dict[str, typing.Union[str, bool]] = {}
+_part1SolutionFunc: typing.Optional[typing.Callable] = None
+_part2SolutionFunc: typing.Optional[typing.Callable] = None
+_solutionOptions: dict[str, typing.Union[str, bool]] = {}
 
 
 def getOption(key: str) -> typing.Union[str, bool, None]:
     """Get the value of a CLI solution option. If not defined, will return `None`."""
-    return solutionOptions.get(key, None)
+    return _solutionOptions.get(key, None)
 
 
 def part1(func: typing.Callable[[str], typing.Any]):
@@ -38,8 +38,8 @@ def part1(func: typing.Callable[[str], typing.Any]):
         return answer
     ```
     """
-    global part1SolutionFunc
-    part1SolutionFunc = func
+    global _part1SolutionFunc
+    _part1SolutionFunc = func
     return func
 
 
@@ -58,8 +58,8 @@ def part2(func: typing.Callable[[str, typing.Any], None]):
         utils.printAnswer(str(answer))
     ```
     """
-    global part2SolutionFunc
-    part2SolutionFunc = func
+    global _part2SolutionFunc
+    _part2SolutionFunc = func
     return func
 
 
@@ -86,24 +86,24 @@ def _cli(
         entrySplit = entry.split("=")
         key = entrySplit[0]
         value = True if len(entrySplit) == 1 else entrySplit[1]
-        solutionOptions[key] = value
+        _solutionOptions[key] = value
 
-    global part1SolutionFunc, part2SolutionFunc
+    global _part1SolutionFunc, _part2SolutionFunc
 
     # Run the part 1 solution
-    if part1SolutionFunc is None:
+    if _part1SolutionFunc is None:
         printError("No solution method for Part 1 was defined!")
     printMessage("Part 1 solution is executing...")
-    part1Result: typing.Any = part1SolutionFunc(copy.copy(inputData))
+    part1Result: typing.Any = _part1SolutionFunc(copy.copy(inputData))
 
     # Run the part 2 solution if desired and it exists
     if part2:
         printMessage("")
-        if part2SolutionFunc is None:
+        if _part2SolutionFunc is None:
             printWarning("No solution method for Part 2 was defined!")
         else:
             printMessage("Part 2 solution is executing...")
-            part2SolutionFunc(copy.copy(inputData), part1Result)
+            _part2SolutionFunc(copy.copy(inputData), part1Result)
 
 
 def start():
